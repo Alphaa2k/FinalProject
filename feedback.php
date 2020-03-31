b910ecd89e3922002dcdcce06a8f95ac5b556815
Alphaa2k <36515896+Alphaa2k@users.noreply.github.com>
2020-03-31 17:48:38 +0100

    Feedback Form

--- feedback.php
+++ feedback.php
@@ -12,47 +12,77 @@
 </head>
 
 <script type="text/javascript">
-document.addEventListener('DOMContentLoaded', function() {
-var elems = document.querySelectorAll('.sidenav');
-var instances = M.Sidenav.init(elems, {});
-});
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+  });
 </script>
 
-<nav>
-  <div class="nav-wrapper purple darken-4">
-    <!-- <a href="#!" class="brand-logo center">Logo</a> -->
-    <ul class="left hide-on-med-and-down">
-      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-      <li><a class="active">Home</a></li>
-      <li><a href="accessibility.php">Accessibility</a></li>
-      <li><a href="feedback.php">Feedback</a></li>
-      <li><a href="#LibraryWeb">Library Website</a></li>
-      <li><a href="#contact">Contact Us</a></li>
-      <li><a href="#help">Help</a></li>
-    </ul>
-    <ul class="right hide-on-med-and-down">
-      <li><a href="login.php" id="account">My Account</a></li>
-    </ul>
-  </div>
-</nav>
+<body>
 
-<ul class="sidenav" id="mobile-demo">
-  <li><a class="active">Home</a></li>
-  <li><a href="accessibility.php">Accessibility</a></li>
-  <li><a href="feedback.php">Feedback</a></li>
-  <li><a href="#LibraryWeb">Library Website</a></li>
-  <li><a href="#contact">Contact Us</a></li>
-  <li><a href="#help">Help</a></li>
-  <li><a href="login.php" id="account">My Account</a></li>
- </ul>
+  <nav>
+    <div class="nav-wrapper purple darken-4">
+      <!-- <a href="#!" class="brand-logo center">Logo</a> -->
+      <ul class="left hide-on-med-and-down">
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        <li><a class="active">Home</a></li>
+        <li><a href="accessibility.php">Accessibility</a></li>
+        <li><a href="feedback.php">Feedback</a></li>
+        <li><a href="#LibraryWeb">Library Website</a></li>
+        <li><a href="#contact">Contact Us</a></li>
+        <li><a href="#help">Help</a></li>
+      </ul>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="login.php" id="account">My Account</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <ul class="sidenav" id="mobile-demo">
+    <li><a href="index.php">Home</a></li>
+    <li><a href="accessibility.php">Accessibility</a></li>
+    <li class="active"><a>Feedback</a></li>
+    <li><a href="#LibraryWeb">Library Website</a></li>
+    <li><a href="#contact">Contact Us</a></li>
+    <li><a href="help.php">Help</a></li>
+    <li><a href="login.php" id="account">My Account</a></li>
+  </ul>
+
+  <div class="container">
+    <h3>Feedback on Library Catalogue</h3>
+
+    <div class="row">
+      <form class="col s12" action="mailto:up848130@myport.ac.uk?subject=Feedback on Library Catalogue&body=You have recieved the following Feedback below." method="POST">
+        <div class="row">
+          <div class="input-field col s6">
+            <input id="full_name" type="text" class="validate" name="full_name">
+            <label for="full_name">Full Name</label>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="input-field col s6">
+            <input id="email" type="email" class="validate" name="email">
+            <label for="email">Email</label>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="input-field col s12">
+            <textarea id="textarea1" class="materialize-textarea" name="comment"></textarea>
+            <label for="textarea1">Your Feedback</label>
+          </div>
+        </div>
+
+        <button type="submit" name="button">Submit Feedback</button>
+        <button type="reset" name="button">Clear Form</button>
+      </form>
+    </div>
+
+    <footer>
+    </footer>
 
 
-  <h3>Feedback on Library Catalogue</h3>
-
-
-  <footer>
-
-  </footer>
 </body>
 
 </html>
